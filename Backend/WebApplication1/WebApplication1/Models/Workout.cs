8f781fda3c4168606017963a4fa274bd8bd398ce
Bende Selmeczi <selmeczi.bende@gmail.com>
2023-10-11 22:03:38 +0200

    Add tables for merch

--- Backend/WebApplication1/WebApplication1/Models/Workout.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models;
-
-public class Workout
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    // Add other properties as needed
-}
