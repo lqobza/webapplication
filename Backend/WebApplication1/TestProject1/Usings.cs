980a912b9934061de60ca1bcfd3601010b6c1c9b
Bende Selmeczi <selmeczi.bende@gmail.com>
2025-04-29 22:20:19 +0200

    [BACKEND] Unit test cleanup

--- Backend/WebApplication1/TestProject1/Usings.cs
+++ Backend/WebApplication1/TestProject1/Usings.cs
@@ -1,8 +1,5 @@
 global using NUnit.Framework;
 global using Moq;
-global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Logging;
-global using WebApplication1.Controllers;
 global using System.Collections.Generic;
-global using Microsoft.AspNetCore.Http;
 global using System.Threading.Tasks;
